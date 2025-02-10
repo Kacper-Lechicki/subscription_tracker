cff58b7931537f57bf6b3c1d2a20258042730572
Kacper Lechicki <klechicki.code@gmail.com>
2025-02-10 22:04:27 +0100

    feat: initial setup v2

---
+++ README.md
@@ -0,0 +1,9 @@
+# 📁 **Name**: subscription_tracker
+
+### 🎯 **Goal**:
+
+This project is designed to practice Node.js / Express.js and BE development skills.
+
+### 🛠️ **Tools**:
+
+Node.js, Express.js, Javascript
--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 import express from "express";
-import "dotenv/config";
+import { PORT } from "./config/env.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API!");
 });
 
-app.listen(port, () => {
-  console.log(`Subscription Tracker API listening on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Subscription Tracker API listening on http://localhost:${PORT}`);
 });
 
 export default app;
